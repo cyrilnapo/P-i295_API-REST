0a00169fb622682a731480ab3a99a1cb0c9dfc09
yohcard <cardisyohan@gmail.com>
2024-03-08 14:39:35 +0100

    POST /api/books/

--- P_WEB295-Ouvrages/routes/books.mjs
+++ P_WEB295-Ouvrages/routes/books.mjs
@@ -54,5 +54,61 @@ booksRouter.get("/:id/notes", (req, res) => {
   res.json(alert(message, roundedAverage));
 });
 
+booksRouter.post("/", async (req, res) => {
+  try {
+    const bookData = {
+      booTitle: req.body.booTitle,
+      booNbPages: req.body.booNbPages,
+      // Ajoutez d'autres champs selon votre modèle
+      booImagecover: req.body.booImagecover,
+    };
+
+    const createdBook = await Book.create(bookData);
+
+    const message = `Le livre ${createdBook.booTitle} a bien été créé !`;
+    res.json(success(message, createdBook));
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      const message = error.errors.map((e) => e.message).join(", ");
+      return res.status(400).json({ message, data: error });
+    }
+
+    const message =
+      "Le livre n'a pas pu être ajouté. Merci de réessayer dans quelques instants.";
+    res.status(500).json({ message, data: error });
+  }
+});
+
+booksRouter.post("/:id/comments", (req, res) => {
+  const bookId = req.params.id;
+
+  const commentData = {
+    content: req.body.content,
+  };
+
+  Comment.create(commentData)
+    .then((createdComment) => {
+      return Book.findByPk(bookId, { include: Comment });
+    })
+    .then((bookWithComments) => {
+      if (!bookWithComments) {
+        const message =
+          "Le livre demandé n'existe pas. Merci de réessayer avec un autre identifiant.";
+        return res.status(404).json({ message });
+      }
+
+      return bookWithComments.addComment(createdComment);
+    })
+    .then(() => {
+      const message = `Le commentaire a bien été ajouté au livre ${bookWithComments.booTitle} !`;
+      res.json(success(message, createdComment));
+    })
+    .catch((error) => {
+      const message =
+        "Une erreur est survenue. Merci de réessayer dans quelques instants.";
+      res.status(500).json({ message, data: error });
+    });
+});
+
 // Export.s
 export { booksRouter };
