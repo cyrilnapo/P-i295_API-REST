6497e800da0c81c48cb59383e80c0f331836b8e9
yohcard <cardisyohan@gmail.com>
2024-03-15 16:20:58 +0100

    Update books.mjs

--- P_WEB295-Ouvrages/routes/books.mjs
+++ P_WEB295-Ouvrages/routes/books.mjs
@@ -23,63 +23,6 @@ booksRouter.get("/:id", (req, res) => {
   res.json(alert(message, book));
 });
 
-booksRouter.get("/:id/notes", (req, res) => {
-  // Retrieving the book id passed as a parameter
-  const bookId = req.params.id;
-  const book = books.find((book) => book.bookId == bookId);
-
-  // Check if the book exists
-  if (!book) {
-    const errorMessage = `Le livre n°${bookId} n'a pas été trouvé`;
-    return res.json(alert(errorMessage, null));
-  }
-
-  // Check if 'notes' property exists on the book
-  if (!book.notes) {
-    const errorMessage = `Le livre n°${bookId} n'a pas de notes`;
-    return res.json(alert(errorMessage, null));
-  }
-
-  // Calculation of the average of the marks given
-  const bookNotes = book.notes;
-  const sumNotes = bookNotes.reduce(
-    (totalNotes, currentNote) => currentNote + totalNotes,
-    0 // Provide an initial value to avoid errors with empty 'notes' array
-  );
-  const averageNotes = sumNotes / bookNotes.length;
-  const roundedAverage = averageNotes.toFixed(2);
-
-  // Alert message
-  const message = `Les notes du livre n°${bookId} ont été récupérées`;
-  res.json(alert(message, roundedAverage));
-});
-
-
-booksRouter.post("/", async (req, res) => {
-  try {
-    const bookData = {
-      booTitle: req.body.booTitle,
-      booNbPages: req.body.booNbPages,
-      // Ajoutez d'autres champs selon votre modèle
-      booImagecover: req.body.booImagecover,
-    };
-
-    const createdBook = await Book.create(bookData);
-
-    const message = `Le livre ${createdBook.booTitle} a bien été créé !`;
-    res.json(success(message, createdBook));
-  } catch (error) {
-    if (error instanceof ValidationError) {
-      const message = error.errors.map((e) => e.message).join(", ");
-      return res.status(400).json({ message, data: error });
-    }
-
-    const message =
-      "Le livre n'a pas pu être ajouté. Merci de réessayer dans quelques instants.";
-    res.status(500).json({ message, data: error });
-  }
-});
-
 booksRouter.post("/:id/comments", (req, res) => {
   const bookId = req.params.id;
 
@@ -109,6 +52,62 @@ booksRouter.post("/:id/comments", (req, res) => {
         "Une erreur est survenue. Merci de réessayer dans quelques instants.";
       res.status(500).json({ message, data: error });
     });
+});
+
+booksRouter.post("/", async (req, res) => {
+  try {
+    const bookData = {
+      booTitle: req.body.booTitle,
+      booNbPages: req.body.booNbPages,
+      // Ajoutez d'autres champs selon votre modèle
+      booImagecover: req.body.booImagecover,
+    };
+
+    const createdBook = await books.create(bookData);
+
+    const message = `Le livre ${createdBook.booTitle} a bien été créé !`;
+    res.json(success(message, createdBook));
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      const message = error.errors.map((e) => e.message).join(", ");
+      return res.status(400).json({ message, data: error });
+    }
+
+    const message =
+      "Le livre n'a pas pu être ajouté. Merci de réessayer dans quelques instants.";
+    res.status(500).json({ message, data: error });
+  }
+});
+
+booksRouter.get("/:id/notes", (req, res) => {
+  // Retrieving the book id passed as a parameter
+  const bookId = req.params.id;
+  const book = books.find((book) => book.bookId == bookId);
+
+  // Check if the book exists
+  if (!book) {
+    const errorMessage = `Le livre n°${bookId} n'a pas été trouvé`;
+    return res.json(alert(errorMessage, null));
+  }
+
+  // Check if 'notes' property exists on the book
+  if (!book.notes) {
+    const errorMessage = `Le livre n°${bookId} n'a pas de notes`;
+    return res.json(alert(errorMessage, null));
+  }
+
+  // Calculation of the average of the marks given
+  const bookNotes = book.notes;
+  const sumNotes = bookNotes.reduce(
+    (totalNotes, currentNote) => currentNote + totalNotes,
+    0 // Provide an initial value to avoid errors with empty 'notes' array
+  );
+  const averageNotes = sumNotes / bookNotes.length;
+  const roundedAverage = averageNotes.toFixed(2);
+
+  // Alert message
+  const message = `Les notes du livre n°${bookId} ont été récupérées`;
+  res.json(alert(message, roundedAverage));
 });
 
 // Export.s
